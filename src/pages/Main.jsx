39098fbc9e8702f28bb5dd3cd4da4358eea4cebc
Muradov Tarik <muradovt111@gmail.com>
2021-01-07 21:47:12 +0300

    well... it works

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,15 +6,20 @@ import { CurrencyRow } from "../components/CurrencyRow";
 
 export const Main = () => {
   // const key = process.env.REACT_APP_API_KEY;
+  const instance = axios.create({
+    baseURL: "https://api.exchangeratesapi.io",
+  });
 
-  const dispatch = useDispatch();
-  const currencyList = useSelector((state) => state.currencyList.rates);
-  const baseCurrency = useSelector((state) => state.currencyList.base);
+  const BASE_URL = "https://api.exchangeratesapi.io/latest";
+
+  // const dispatch = useDispatch();
+  // const currencyList = useSelector((state) => state.currencyList.rates);
+  // const baseCurrency = useSelector((state) => state.currencyList.base);
 
   const [currencyOptions, setCurrencyOptions] = React.useState([]);
   const [fromCurrency, setFromCurrency] = React.useState();
   const [toCurrency, setToCurrency] = React.useState();
-  const [amount, setAmount] = React.useState(1);
+  const [amount, setAmount] = React.useState();
   const [amountInFromCurrency, setAmountInFromCurrency] = React.useState(true);
   const [exchangeRate, setExchangeRate] = React.useState();
   console.log(exchangeRate, "set exchange rate");
@@ -30,23 +35,48 @@ export const Main = () => {
   }
 
   React.useEffect(() => {
-    const veryFirstCurrency = Object.keys(currencyList)[0];
+    // const veryFirstCurrency = Object.keys(currencyList)[0];
     // console.log(veryFirstCurrency, "console.log in useEffect");
-    dispatch(getAllCurrencyPairs(dispatch));
+    // dispatch(getAllCurrencyPairs(dispatch));
     // getAllCurrencyPairs(dispatch);
-    setCurrencyOptions([baseCurrency, ...Object.keys(currencyList)]);
-    setFromCurrency(baseCurrency);
-    setToCurrency(veryFirstCurrency);
-    setExchangeRate(currencyList[veryFirstCurrency]);
-  }, [dispatch]);
+    // axios.get(`${BASE_URL}/latest`).then((response) => {
+    //   console.log(response, "use effect data");
+    //   const firstCurrency = Object.keys(data.rates)[0];
+    //   setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+    //   setFromCurrency(data.base);
+    //   setToCurrency(firstCurrency);
+    //   setExchangeRate(data.rates[firstCurrency]);
+
+    //   console.log(Object.keys(data.rates), "Keys in main");
+    // });
+    // setCurrencyOptions([baseCurrency, ...Object.keys(currencyList)]);
+    // setFromCurrency(baseCurrency);
+    // setToCurrency(veryFirstCurrency);
+    // setExchangeRate(currencyList[veryFirstCurrency]);
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+      });
+  }, []);
+
+  // React.useEffect(() => {
+  //   if (fromCurrency !== null && toCurrency !== null) {
+  //     axios
+  //       .get(`latest?base=${fromCurrency}&symbols=${toCurrency}`)
+  //       .then((data) => setExchangeRate(data.rates[toCurrency]));
+  //   }
+  // }, [fromCurrency, toCurrency]);
 
   React.useEffect(() => {
-    if (fromCurrency !== null && toCurrency !== null) {
-      axios
-        .get(`https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`)
-        .then((response) => {
-          console.log(response.data, "response data in second useEffect");
-        });
+    if (fromCurrency && toCurrency) {
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]));
     }
   }, [fromCurrency, toCurrency]);
 
