8393958c95a64e3598c8aae262117e78893c6674
Muradov Tarik <muradovt111@gmail.com>
2021-01-09 12:07:08 +0300

    for now it just show currency rates and removed decoration for LI tag

--- src/pages/CurrencyRates.jsx
+++ src/pages/CurrencyRates.jsx
@@ -2,20 +2,39 @@ import axios from "axios";
 import React from "react";
 
 export const CurrencyRates = () => {
-  //   const [getAllCurrencyRatesValues, setAllCurrencyRateValues] = React.useState([]);
-  //   const [getAllCurrencyRatesKeys, setAllCurrencyRateKeys] = React.useState([]);
-  //   console.log(getAllCurrencyRatesValues);
-  //   console.log(getAllCurrencyRatesKeys);
+  const [getAllCurrencyRatesValues, setAllCurrencyRateValues] = React.useState([]);
+  const [getAllCurrencyRatesKeys, setAllCurrencyRateKeys] = React.useState([]);
+  console.log(getAllCurrencyRatesValues);
+  console.log(getAllCurrencyRatesKeys);
   React.useEffect(() => {
     axios.get("https://api.exchangeratesapi.io/latest").then(({ data }) => {
       //   console.log(data.rates);
       //   setAllCurrencyRates(Object.values(data.rates));
-      //   setAllCurrencyRateValues([...Object.values(data.rates)]);
-      //   setAllCurrencyRateKeys([...Object.keys(data.rates)]);
+      setAllCurrencyRateValues([...Object.values(data.rates)]);
+      setAllCurrencyRateKeys([...Object.keys(data.rates)]);
       //   setAllCurrencyRates(Object.values(data.rates), ...Object.values(data.keys));
       const currencyList = data.rates;
-      console.log(currencyList);
+      // console.log(currencyList);
     });
   }, []);
-  return <div>{}</div>;
+  return (
+    <div className="Currency-rates-page">
+      <div>
+        {
+          <ul>
+            {getAllCurrencyRatesValues.map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        }
+      </div>
+      <div>
+        <ul>
+          {getAllCurrencyRatesKeys.map((value) => (
+            <li key={value}>{value}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
